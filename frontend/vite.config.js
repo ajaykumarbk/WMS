7ea543bfb9b5c238b30b7d43a48138981910d697
ajaykumarbk <ajaykumarbk34@gmail.com>
2025-11-01 05:17:55 +0000

    adding docker files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:5000',
+//       '/uploads': 'http://localhost:5000'
+//     }
+//   }
+// })
+
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
-      '/api': 'http://localhost:5000',
-      '/uploads': 'http://localhost:5000'
+      '/api': {
+        target: 'http://backend:5000',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://backend:5000',
+        changeOrigin: true
+      }
     }
   }
 })
