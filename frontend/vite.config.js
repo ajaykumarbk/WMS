76f84375663502014580094debc752fb44a0c8df
ajaykumarbk <ajaykumarbk34@gmail.com>
2025-11-13 11:49:35 +0530

    k8s files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://backend:5000',
-      '/uploads': 'http://backend:5000'
+      '/api': 'http://localhost:5000',
+      '/uploads': 'http://localhost:5000'
     }
   }
 })
